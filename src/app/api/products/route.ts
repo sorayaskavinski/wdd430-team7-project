f3c84ec8998bb758cd14eea5aa5dcf8849f83fac
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2025-09-29 18:46:17 -0600

    Another attempt

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient, Category } from "@/generated/prisma";
+import { PrismaClient, Category } from '../../../generated/prisma';
 
 const prisma = new PrismaClient();
 
